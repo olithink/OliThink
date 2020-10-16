2177e2a38b037d2dbb842464e684a0b1b584e134
Oliver Brausch <brausch@nuclos.de>
2020-10-17 01:54:06 +0200

    5.8.7 Fix incorrect Bioskey for Windows Src

--- src/olithink.c
+++ src/olithink.c
@@ -365,7 +365,26 @@ u64 getTime() {
 }
 
 int bioskey() {
-	return _kbhit();
+	static int init = 0, pipe;
+	static HANDLE inh;
+	DWORD dw;
+
+	if (!init) {
+		init = 1;
+		inh = GetStdHandle(STD_INPUT_HANDLE);
+		pipe = !GetConsoleMode(inh, &dw);
+		if (!pipe) {
+			SetConsoleMode(inh, dw & ~(ENABLE_MOUSE_INPUT|ENABLE_WINDOW_INPUT));
+			FlushConsoleInputBuffer(inh);
+		}
+	}
+	if (pipe) {
+		if (!PeekNamedPipe(inh, NULL, 0, NULL, &dw, NULL)) return 1;
+		return dw;
+	} else {
+		GetNumberOfConsoleInputEvents(inh, &dw);
+		return dw <= 1 ? 0 : dw;
+	}
 }
 #else
 u64 getTime() {
